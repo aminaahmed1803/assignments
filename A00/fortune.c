5cd30af546adf2fb167f519227579c1f4f48d9ee
Amina <79462671+aminaahmed1803@users.noreply.github.com>
2022-02-18 20:06:57 -0500

    Add files via upload

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * fortune.c
+ * Author: Amina AHmed
+ * Date: 1/21/2022
+ * Print A fortne
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("The journey of a 1000 steps atarts with getting out of bed\n");
+  return 0;
+}
