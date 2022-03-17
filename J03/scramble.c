0f9cbc1323b07135c9eebc387cb739c1897f53cb
Amina Ahmed <aminaahmed@wc118-dhcp-47.brynmawr.edu>
2022-03-17 15:16:38 -0400

    Jam submission

--- J03/scramble.c
+++ J03/scramble.c
@@ -1,3 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int swapBits(int a)
+{
+  int n = a;
+
+  int first = n%2;
+  n = n/2;
+  int sec = n%2; 
+  n = n/2;
+  int third = n%2;
+  n = n/2;
+
+  if (sec == 1)
+    n = (n*2) + 1;
+  else 
+    n = n*2; 
+
+  if (third == 1)
+    n = (n*2) + 1;
+  else 
+    n = n*2; 
+
+  if (first == 1)
+    n = (n*2) + 1;
+  else 
+    n = n*2; 
+
+  return n;
+}
+
 int main() {
+
+  char message[100]; 
+  printf("Please enter a phrase: ");
+  fgets(message, 100, stdin);
+
+  for (int i=0 ; message[i]!='\0' ; i++)
+  {
+    int temp = message[i];
+    temp = swapBits(temp);
+    message[i] = temp;
+
+  }
+  printf("\n%s", message);
   return 0;
 }
