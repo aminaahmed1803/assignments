7923cf21b633e5ae1c55764421dc89552bf7c117
Amina Ahmed <aminaahmed@wc118-dhcp-47.brynmawr.edu>
2022-04-21 15:25:53 -0400

    submit

--- J05/dot.c
+++ J05/dot.c
@@ -6,6 +6,23 @@
 
 #define SIZE 100000
 
+struct threadData {
+  int* u;
+  int* v;
+  int start;
+  int stop;
+  int dp;
+};
+
+void dotprod(void* threadData_i) {
+  struct threadData * data = (struct threadData*) threadData_i;
+  printf("start: %d, stop: %d\n", data->start, data->stop);
+  for (int i = data->start; i < data->stop; i++) {
+    data->dp = data->dp + (data->u[i] * data->v[i]);
+  }
+  printf("dp: %d\n",data->dp);
+}
+
 int main(int argc, char *argv[]) {
   srand(time(0));
 
@@ -16,12 +33,30 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < SIZE; i++) {
     v[i] = rand() % 1000 - 500;
     u[i] = rand() % 1000 - 500;
-    dotproduct += u[i] * v[i];
+    dotproduct = u[i] * v[i];
   }
   printf("Ground truth dot product: %d\n", dotproduct);
 
   // TODO: Implement your thread solution here
-  printf("Test with 4 threads\n");
+  // int dp = 0;
+  pthread_t* tids = malloc(sizeof(pthread_t) * 4);
+  struct threadData* dataArr = malloc(sizeof(struct threadData) * 4);
+  for (int i=0; i < 4; i++) {
+    pthread_t thread = NULL;
+    tids[i] = thread;
+    struct threadData threadData_i = {u, v, i*(SIZE/4), (i+1)*(SIZE/4), 0};
+    dataArr[i] = threadData_i;
+    pthread_create(&tids[i], NULL, (void*) dotprod, (void*) &dataArr[i]);
+  }
 
+  int dotproduct2 = 0;
+  for (int i=0; i < 4; i++) {
+    pthread_join(tids[i], NULL);
+    dotproduct2 += dataArr[i].dp;
+  }
+  
+  printf("Test with 4 threads\n");
+  
+  printf("Answer with threads: %d\n", dotproduct2);
   return 0;
 }
